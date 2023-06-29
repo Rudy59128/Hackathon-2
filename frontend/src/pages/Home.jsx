c044a2de562e9851149b82ef9c4970b4e004783a
wilder <henno.ambre@gmail.com>
2023-06-29 16:21:58 +0200

    create login page left background image login form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,44 @@
-export default function Home() {
+import React from "react";
+import "./Home.css";
+
+function Home() {
   return (
-    <header className="App-header">
-      <p>Page de connexion ici</p>
-    </header>
+    <div className="register-photo">
+      <div className="form-container">
+        <div className="image-holder" />
+        <form method="post">
+          <div className="text-pic" />
+          <h2 className="text-center">
+            <strong>CONNEXION</strong>
+          </h2>
+
+          <div className="form-group">
+            <div className="form-group-email">
+              <label>
+                Email
+                <input className="form-control" type="email" name="email" />
+              </label>
+            </div>
+            <div className="form-group-password">
+              <label>
+                Mot de passe
+                <input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                />
+              </label>
+            </div>
+            <div className="form-group-button">
+              <button className="btn" type="submit">
+                Se connecter
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
+
+export default Home;
